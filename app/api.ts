acd58b245e1f9c52a5eac478300a5a3f1696fd11
ManaTeWai <maksimts03@gmail.com>
2024-01-29 13:21:51 +0300

    nexr fix

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,7 @@
 export const API = {
 	topPage: {
 		find: process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
-		byAlias: process.env.NEXR_PUBLIC_DOMAIN + '/api/top-Page/byAlias'
+		byAlias: process.env.NEXT_PUBLIC_DOMAIN + '/api/top-Page/byAlias'
 	},
 	product: {
 		find: process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find'
